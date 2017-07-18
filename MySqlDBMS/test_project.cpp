60112a94a84d86cdfa3ab33231fb963bb5ff26dd
ChenHao <1093508079@qq.com>
2017-07-18 12:10:08 +0800

    main主程序完成
    main主程序完成

--- MySqlDBMS/test_project.cpp
+++ MySqlDBMS/test_project.cpp
@@ -8,6 +8,43 @@ using std::stringstream;
 using std::string;
 using std::vector;
 
-int main(){
 
+void showInsert(unsigned countLine, string tableName){
+	if(tableName == "don't show")
+		return;
+	cout<<"执行插入: "<<countLine<<" 行受影响";
+	//	<<"(0x"<<std::hex<<countLine<<std::dec<<")\n";
+	cout<<(global::gTableSpace(tableName).screen(tableName,"\t"))
+		<<endl;
+}
+
+int main(){
+	auto scanIBDATA1 = TableScan(); 
+	try{
+		scanIBDATA1.open();
+		scanIBDATA1.getNext();
+	}
+	catch(long long hex){
+		std::cerr<<"ErrorCode (错误代码) : "<<std::hex<<hex<<std::dec<<endl;
+		if(hex==0xbadbadbadbad07)
+			std::cerr<<"目前迭代器不支持non-exist-segments[\"data\"]的表\n\n";
+		else
+			throw hex;
+	}
+	catch(const char* ex){
+		std::cerr<<"ErrorCode (错误代码) : "<<ex<<endl;
+		if(std::string(ex) == std::string("map-key non-exist"))
+			std::cerr<<"目前迭代器不支持non-exist-segments[\"data\"]的表\n\n";
+		else
+			throw ex;
+	}
+	
+	string tableName = "ini";
+	unsigned countLine = global::iniTableInsert();
+	showInsert(countLine, "don't show");
+	
+	auto scanIni = TableScan(tableName); 
+	scanIni.open();
+	for(int i=0;i<70*5;i++) 
+		scanIni.getNext()->screen();
 }
